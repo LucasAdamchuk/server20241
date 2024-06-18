f4ff2aec7691d289494747418700716208cfb2f6
LucasAdamchuk <123332542+LucasAdamchuk@users.noreply.github.com>
2024-06-18 20:01:26 +0000

    commitando alteração

--- controllers/course.ts
+++ controllers/course.ts
@@ -13,17 +13,17 @@ const courses = [
 ]
 export async function listCourse(req: Request, res: Response) {
   //conecta com o banco
-  //const client = await pool.connect();
+  const client = await pool.connect();
   //realiza consulta sql
-  //const courses = await client.query(`select * from courses`)
+  const courses = await client.query(`select * from courses`)
   //retorna consulta em formato json
-  return res.status(200).json(courses);
+  return res.status(200).json(courses.rows);
 }
 export async function saveCourse(req: Request, res: Response) {
   const course = req.body;
-  course.id = Math.floor(Math.random() * 50)
-  courses.push(course)
-  res.status(201).json(courses);
+  const client = await pool.connect();
+  const response = await client.query(`insert into courses (name) values ('${course.name}')`)
+  res.status(201).json(response);
 }
 
 
