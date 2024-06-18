7e9d6282776aac15443a09000454607d13613c2b
LucasAdamchuk <123332542+LucasAdamchuk@users.noreply.github.com>
2024-06-18 23:17:24 +0000

    arrumando back as universidades

--- controllers/university.ts
+++ controllers/university.ts
@@ -5,14 +5,14 @@ export async function listuniversity(req: Request, res: Response) {
   //conecta com o banco
   const client = await pool.connect();
   //realiza consulta sql
-  const courses = await client.query(`select * from courses`)
+  const universitys = await client.query(`select * from university`)
   //retorna consulta em formato json
-  return res.status(200).json(courses.rows);
+  return res.status(200).json(universitys.rows);
 }
 export async function saveuniversity(req: Request, res: Response) {
-  const course = req.body;
+  const university = req.body;
   const client = await pool.connect();
-  const response = await client.query(`insert into courses (name) values ('${course.name}')`)
+  const response = await client.query(`insert into university (nome, endereco) values ('${university.name}', '${university.endereco}')`)
   res.status(201).json(response);
 }
 
